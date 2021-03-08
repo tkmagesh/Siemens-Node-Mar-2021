c3d58b2870abffb029d7baa8c9b804d9d18c1204
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-08 16:52:46 +0530

    print file - async

---
+++ 04-print-file/print-file-async.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+
+fs.readFile('./sample1.txt', { encoding : 'utf8' }, function(err, fileContents){
+    if (err){
+        console.log('Something went wrong');
+        console.log(err);
+        return;
+    }
+    console.log(fileContents);
+    console.log('Thats all folks!!');
+});
