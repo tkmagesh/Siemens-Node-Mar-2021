55534be947fae95789b31df020cfb9a40c0dff34
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-09 11:48:26 +0530

    ignoring querystrings to get the pathname

--- 05-web-server/server.js
+++ 05-web-server/server.js
@@ -1,10 +1,12 @@
 var http = require('http'),
     path = require('path'),
     fs = require('fs');
+   
 
 var server = http.createServer(function(req /* IncomingMessage */, res /* ServerResponse */){
-    console.log(req.method + ' - ' + req.url);
-    var resourceName = req.url === '/' ? '/index.html' : req.url;
+    var urlObj = new URL(req.url, `http://${req.headers.host}`)
+    console.log(req.method + ' - ' + urlObj.pathname);
+    var resourceName = urlObj.pathname === '/' ? '/index.html' : urlObj.pathname;
     var resourceFullName = path.join(__dirname, resourceName);
     if (!fs.existsSync(resourceFullName)){
         res.statusCode = 404;
