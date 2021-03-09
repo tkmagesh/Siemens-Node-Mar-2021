abcc1a0e43d3354edce3ea4c5a1434abb84970b5
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-09 15:46:56 +0530

    async seq demo complete

--- 09-async-seq/async-demos.js
+++ 09-async-seq/async-demos.js
@@ -18,48 +18,92 @@ function f4Sync(){
     console.log('f4Sync completed');
 }
 
+/* 
 function runSync(){
     f1Sync();
     f2Sync();
     f3Sync();
     f4Sync();
+} 
+*/
+
+function runSync(){
+    var syncFns = [ f1Sync, f2Sync, f3Sync, f4Sync ];
+    for(var i=0; i<syncFns.length; i++){
+        var fn = syncFns[i];
+        fn();
+    }
 }
+
 
 module.exports['runSync'] = runSync;
 
-function f1Async(){
+function f1Async(next){
     console.log('f1Async triggered');
     setTimeout(function(){
         console.log('f1Async completed');
+        if (typeof next === 'function')
+            next();
     }, 6000);
 }
 
-function f2Async(){
+function f2Async(next){
     console.log('f2Async triggered');
     setTimeout(function(){
         console.log('f2Async completed');
+        if (typeof next === 'function')
+            next();
     }, 2000);
 }
 
-function f3Async(){
+function f3Async(next){
     console.log('f3Async triggered');
     setTimeout(function(){
         console.log('f3Async completed');
+        if (typeof next === 'function')
+            next();
     }, 4000);
 }
 
-function f4Async(){
+function f4Async(next){
     console.log('f4Async triggered');
     setTimeout(function(){
         console.log('f4Async completed');
+        if (typeof next === 'function')
+            next();
     }, 5000);
 }
 
+/* 
 function runAsync(){
-    f1Async();
-    f2Async();
-    f3Async();
-    f4Async();
+    f1Async(function(){
+        f2Async(function(){
+            f3Async(function(){
+                f4Async();
+            });
+        });
+    });
+} 
+*/
+/* 10, 20,30,40
+10 + sum(20,30,40)
+        => 20 + sum(30,40)
+                => 30 + sum(40)
+                        => 40 */
+
+function exec(fns){
+    var first = fns[0],
+        remaining = fns.slice(1),
+        next = function(){
+            exec(remaining);
+        };
+    if (typeof first === 'function')
+        first(next);
+}
+
+function runAsync(){
+    var asyncFns = [ f1Async, f2Async, f3Async, f4Async ];
+    exec(asyncFns); 
 }
 
 module.exports['runAsync'] = runAsync;
